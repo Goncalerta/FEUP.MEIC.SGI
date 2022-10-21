d951a86451ff1a8e9a2ee5e09c1fb0ac414452b1
“Pedro <up201905348@edu.fe.up.pt>
2022-10-21 22:39:17 +0100

    remove debugging console log

--- tp1/MySceneGraph.js
+++ tp1/MySceneGraph.js
@@ -77,7 +77,6 @@ export class MySceneGraph {
         // As the graph loaded ok, signal the scene so that any additional initialization
         // depending on the graph can take place
         this.scene.onGraphLoaded();
-        console.log(this)
     }
 
     /**
