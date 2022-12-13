5ee67f3eead3dd1db8105dfbbc5b4ca0aa26c14d
Diogo Costa <up201906731@edu.fe.up.pt>
2022-12-13 12:24:28 +0000

    Add display of all player's checkers

--- tp3/game/MyChecker.js
+++ tp3/game/MyChecker.js
@@ -7,6 +7,17 @@ import { MyCylinder } from '../MyCylinder.js';
  * MyChecker class, representing a checker.
  */
 export class MyChecker extends CGFobject {
+    MATERIAL = {
+        shininess: 10,
+        emission:  [1.0, 1.0, 1.0, 1.0],
+        ambient:   [0.0, 0.0, 0.0, 1.0],
+        diffuse:   [0.0, 0.0, 0.0, 1.0],
+        specular:  [0.0, 0.0, 0.0, 1.0]
+    };
+
+    TOP_TEXTURE = "scenes/images/game/crown_test.png";
+    CYLINDER_TEXTURE = "scenes/images/space/wood_barrel_side.jpg";
+
     /**
      * @method constructor
      * @param {CGFscene} scene - Reference to MyScene object
@@ -15,7 +26,7 @@ export class MyChecker extends CGFobject {
      * @param {integer} slices - Number of divisions around the Z axis (circumference)
      * @param {integer} stacks - Number of divisions along the Z axis
      */
-    constructor(scene, radius, height, slices, stacks) {
+    constructor(scene, radius, height, slices=50, stacks=10) {
         super(scene);
 
         this.radius = radius;
@@ -26,18 +37,8 @@ export class MyChecker extends CGFobject {
         this.cylinder = new MyCylinder(scene, radius, radius, height, slices, stacks);
         this.circle = new MyCircle(scene, radius, [0, 0, 0], slices);
 
-        // TODO just to test, remove later (?) vv
-        this.material = {
-            shininess: 10,
-            emission: [1.0, 1.0, 1.0, 1.0],
-            ambient: [0.0, 0.0, 0.0, 1.0],
-            diffuse: [0.0, 0.0, 0.0, 1.0],
-            specular: [0.0, 0.0, 0.0, 1.0],
-        };
-        
-        this.topsTexture = new CGFtexture(this.scene, "scenes/images/game/crown_test.png");
-        this.cylinderTexture = new CGFtexture(this.scene, "scenes/images/space/wood_barrel_side.jpg");
-        // ^^
+        this.topsTexture = new CGFtexture(this.scene, this.TOP_TEXTURE);
+        this.cylinderTexture = new CGFtexture(this.scene, this.CYLINDER_TEXTURE);
     }
 
     /**
@@ -46,7 +47,7 @@ export class MyChecker extends CGFobject {
     display() {
         this.scene.pushMatrix();
 
-        this.scene.pushAppearance(this.material, this.topsTexture);
+        this.scene.pushAppearance(this.MATERIAL, this.topsTexture);
         this.scene.translate(0, this.height, 0);
         this.scene.rotate(Math.PI / 2, 1, 0, 0);
 
@@ -63,7 +64,7 @@ export class MyChecker extends CGFobject {
         this.scene.popAppearance();
 
         this.scene.pushMatrix();
-        this.scene.pushAppearance(this.material, this.cylinderTexture);
+        this.scene.pushAppearance(this.MATERIAL, this.cylinderTexture);
         this.cylinder.display();
         this.scene.popAppearance();
         this.scene.popMatrix();
--- tp3/game/MyGame.js
+++ tp3/game/MyGame.js
@@ -1,23 +1,12 @@
 import { MyBoard } from './MyBoard.js';
-import { MyChecker } from './MyChecker.js';
 
 export class MyGame {
     constructor(scene) {
         this.scene = scene;
         this.board = new MyBoard(scene, 0.5);
-
-        // TODO just to test, remove later vv
-        this.checker = new MyChecker(scene, 2, 1, 50, 10);        
-        // ^^
     }
 
     display() {
         this.board.display();
-
-        // TODO just to test, remove later vv
-        this.scene.pushMatrix();
-        this.checker.display();
-        this.scene.popMatrix();
-        // ^^
     }
 }
