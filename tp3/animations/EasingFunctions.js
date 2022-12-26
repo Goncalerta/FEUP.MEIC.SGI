dddfebc743ba18eae5999ccebf4efcd7459194e7
Diogo Costa <up201906731@edu.fe.up.pt>
2022-12-26 23:58:04 +0000

    Add buttons

--- tp3/animations/EasingFunctions.js
+++ tp3/animations/EasingFunctions.js
@@ -2,6 +2,10 @@ export function identity(t) {
     return t;
 }
 
+export function quad(a, h, k) {
+    return (t) => a*(t-h)**2 + k;
+}
+
 // TODO maybe this can be better expressed with a loopback animation? (maybe not because of the place it is used in)
 export function smoothPeak(t) {
     return 1.27325359122*Math.exp(-5*Math.abs(t-0.5)**1.7) - 0.27325359122;
@@ -9,7 +13,7 @@ export function smoothPeak(t) {
 
 // TODO maybe this can be better expressed with an animation chain
 export function quadPeak(t) {
-    return -4 * (t - 0.5)**2 + 1;
+    return quad(-4, 0.5, 1)(t);
 }
 
 // TODO maybe this can be better expressed with an animation chain? (but would that be worth it?) (maybe not because of the place it is used in)
@@ -53,4 +57,11 @@ export function gravityUp(g) {
 
 export function gravityDown(g) {
     return (t) => 1 / (1 + g * (1 - t)**2);
+}
+
+export function quadMin(min) {
+    const h = 0.5;
+    const k = min;
+    const a = (1 - k) / h**2;
+    return (t) => quad(a, h, k)(t);
 }
--- tp3/game/MyScoreBoardBox.js
+++ tp3/game/MyScoreBoardBox.js
@@ -1,10 +1,10 @@
-import { CGFobject, CGFtexture } from "../../lib/CGF.js";
+import { CGFtexture } from "../../lib/CGF.js";
 import { getAppearance } from '../utils.js';
-import { MyRectangle } from "../MyRectangle.js";
+import { MyBox } from "../MyBox.js";
 
-export class MyScoreBoardBox extends CGFobject {
-    TEXTURE_PATH = "scenes/images/game/wood.jpg";
-    MATERIAL = {
+export class MyScoreBoardBox extends MyBox {
+    static TEXTURE_PATH = "scenes/images/game/wood.jpg";
+    static MATERIAL = {
         shininess: 10,
         emission: [0.3, 0.24, 0.24, 1.0],
         ambient: [0.7, 0.6, 0.6, 1.0],
@@ -13,55 +13,9 @@ export class MyScoreBoardBox extends CGFobject {
     };
 
     constructor(scene) {
-        super(scene);
+        const texture = new CGFtexture(scene, MyScoreBoardBox.TEXTURE_PATH);
+        const appearance = getAppearance(scene, MyScoreBoardBox.MATERIAL, texture);
 
-        this.quad = new MyRectangle(scene, -0.5, 0.5, -0.5, 0.5);
-        this.texture = new CGFtexture(scene, this.TEXTURE_PATH);
-        this.appearance = getAppearance(scene, this.MATERIAL, this.texture);
-    }
-
-    display() {
-        this.appearance.apply();
-
-        // top
-        this.scene.pushMatrix();
-        this.scene.translate(0, 0.5, 0);
-        this.scene.rotate(-Math.PI/2, 1, 0, 0);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        // bottom
-        this.scene.pushMatrix();
-        this.scene.translate(0, -0.5, 0);
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        // left
-        this.scene.pushMatrix();
-        this.scene.translate(0, 0, 0.5);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        // right
-        this.scene.pushMatrix();
-        this.scene.translate(0, 0, -0.5);
-        this.scene.rotate(Math.PI, 1, 0, 0);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        // front
-        this.scene.pushMatrix();
-        this.scene.translate(0.5, 0, 0);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.quad.display();
-        this.scene.popMatrix();
-
-        // back
-        this.scene.pushMatrix();
-        this.scene.translate(-0.5, 0, 0);
-        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
-        this.quad.display();
-        this.scene.popMatrix();
+        super(scene, appearance);
     }
 }
