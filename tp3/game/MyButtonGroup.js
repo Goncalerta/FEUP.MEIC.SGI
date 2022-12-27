bc5378449571c280c3d727f2ee2261d0bae2e67e
Diogo Costa <up201906731@edu.fe.up.pt>
2022-12-27 17:19:10 +0000

    Refactor button group

---
+++ tp3/game/MyButtonGroup.js
@@ -0,0 +1,50 @@
+import { CGFobject } from "../../lib/CGF.js";
+import { MyButton } from "./MyButton.js";
+
+export class MyButtonGroup extends CGFobject {
+    UNDO_TEXTURE_PATH = "scenes/images/game/undo.png";
+    REDO_TEXTURE_PATH = "scenes/images/game/redo.png";
+    SCENARIO_TEXTURE_PATH = "scenes/images/game/scenario.png";
+    P1_SCENARIO_TEXTURE_PATH = "scenes/images/game/p1_scenario.png";
+    P2_SCENARIO_TEXTURE_PATH = "scenes/images/game/p2_scenario.png";
+    PLAY_TEXTURE_PATH = "scenes/images/game/play.png";
+
+    constructor(scene, buttonWidth, buttonHeight, buttonDepth, buttonColorRGBA) {
+        super(scene);
+
+        this.buttonWidth = buttonWidth;
+        this.buttonHeight = buttonHeight;
+        
+        // TODO use proper callbacks
+        let pickingId = 401;
+        this.undoButton =  new MyButton(scene, pickingId++, () => {}, buttonWidth, buttonHeight, buttonDepth, this.UNDO_TEXTURE_PATH, buttonColorRGBA);
+        this.redoButton =  new MyButton(scene, pickingId++, () => {}, buttonWidth, buttonHeight, buttonDepth, this.REDO_TEXTURE_PATH, buttonColorRGBA);
+        this.povButton =   new MyButton(scene, pickingId++, () => {}, buttonWidth, buttonHeight, buttonDepth, this.SCENARIO_TEXTURE_PATH, buttonColorRGBA);
+        this.playButton =  new MyButton(scene, pickingId++, () => {}, buttonWidth, buttonHeight, buttonDepth, this.PLAY_TEXTURE_PATH, buttonColorRGBA);
+        this.p1PovButton = new MyButton(scene, pickingId++, () => {}, buttonWidth, buttonHeight, buttonDepth, this.P1_SCENARIO_TEXTURE_PATH, buttonColorRGBA);
+        this.p2PovButton = new MyButton(scene, pickingId++, () => {}, buttonWidth, buttonHeight, buttonDepth, this.P2_SCENARIO_TEXTURE_PATH, buttonColorRGBA);
+        // ^^
+
+        this.buttons = [
+            [this.povButton, this.playButton],
+            [this.undoButton, this.redoButton],
+            [this.p1PovButton, this.p2PovButton]
+        ];
+    }
+
+    display() {
+        this.scene.pushMatrix();
+        this.scene.translate(0, 1.1 * this.buttonHeight, 0);
+        for (let i = 0; i < this.buttons.length; i++) {
+            const row = this.buttons[i];
+            for (let j = 0; j < row.length; j++) {
+                const button = row[j];
+                button.display();
+                this.scene.translate(1.1 * this.buttonWidth, 0, 0);
+            }
+            this.scene.translate(-1.1 * this.buttonWidth * row.length, -1.1 * this.buttonHeight, 0);
+        }
+
+        this.scene.popMatrix();
+    }
+}
--- tp3/game/MyScoreBoard.js
+++ tp3/game/MyScoreBoard.js
@@ -3,16 +3,9 @@ import { MyFont } from './MyFont.js';
 import { secondsToFormattedTime } from '../utils.js';
 import { PlayerTurnState } from './GameState.js';
 import { MyScoreBoardBox } from './MyScoreBoardBox.js';
-import { MyButton } from './MyButton.js';
+import { MyButtonGroup } from './MyButtonGroup.js';
 
 export class MyScoreBoard extends CGFobject {
-    UNDO_TEXTURE_PATH = "scenes/images/game/undo.png";
-    REDO_TEXTURE_PATH = "scenes/images/game/redo.png";
-    SCENARIO_TEXTURE_PATH = "scenes/images/game/scenario.png";
-    P1_SCENARIO_TEXTURE_PATH = "scenes/images/game/p1_scenario.png";
-    P2_SCENARIO_TEXTURE_PATH = "scenes/images/game/p2_scenario.png";
-    PLAY_TEXTURE_PATH = "scenes/images/game/play.png";
-
     MAX_CHAR_NAME = 5;
     MAX_SCORE_SIZE = 2;
     BUTTONS_HEIGHT = 0.5;
@@ -35,15 +28,7 @@ export class MyScoreBoard extends CGFobject {
         this.font = new MyFont(scene, 0.01, this.TEXT_COLOR_RGBA);
         this.box = new MyScoreBoardBox(scene);
         
-        // TODO use proper callbacks
-        let pickingId = 401;
-        this.undoButton = new MyButton(scene, pickingId++, () => {}, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.UNDO_TEXTURE_PATH, this.TEXT_COLOR_RGBA);
-        this.redoButton = new MyButton(scene, pickingId++, () => {}, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.REDO_TEXTURE_PATH, this.TEXT_COLOR_RGBA);
-        this.povButton = new MyButton(scene, pickingId++, () => {}, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.SCENARIO_TEXTURE_PATH, this.TEXT_COLOR_RGBA);
-        this.playButton = new MyButton(scene, pickingId++, () => {}, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.PLAY_TEXTURE_PATH, this.TEXT_COLOR_RGBA);
-        this.p1PovButton = new MyButton(scene, pickingId++, () => {}, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.P1_SCENARIO_TEXTURE_PATH, this.TEXT_COLOR_RGBA);
-        this.p2PovButton = new MyButton(scene, pickingId++, () => {}, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.P2_SCENARIO_TEXTURE_PATH, this.TEXT_COLOR_RGBA);
-        // ^^
+        this.buttonGroup = new MyButtonGroup(scene, this.BUTTONS_WIDTH, this.BUTTONS_HEIGHT, this.BUTTONS_DEPTH, this.TEXT_COLOR_RGBA);
 
         this.player1NameShort = player1.getName().substring(0, this.MAX_CHAR_NAME);
         this.player2NameShort = player2.getName().substring(0, this.MAX_CHAR_NAME);
@@ -96,39 +81,7 @@ export class MyScoreBoard extends CGFobject {
         this.scene.translate(translatedAmount, 0, this.depth / 2);
         this.scene.scale(this.width/2, 1, 1);
 
-        this.scene.pushMatrix();
-        this.scene.translate(0, 1.1*this.BUTTONS_HEIGHT, 0);
-        // pov
-        this.povButton.display();
-        
-        this.scene.popMatrix();
-        // undo
-        this.undoButton.display();
-
-        this.scene.pushMatrix();
-        this.scene.translate(0, -1.1*this.BUTTONS_HEIGHT, 0);
-        // p1
-        this.p1PovButton.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.translate(1.1*this.BUTTONS_WIDTH, 0, 0);
-        
-        this.scene.pushMatrix();
-        this.scene.translate(0, 1.1*this.BUTTONS_HEIGHT, 0);
-        // play
-        this.playButton.display();
-        this.scene.popMatrix();
-        // redo
-        this.redoButton.display();
-
-        this.scene.pushMatrix();
-        this.scene.translate(0, -1.1*this.BUTTONS_HEIGHT, 0);
-        // p2
-        this.p2PovButton.display();
-        this.scene.popMatrix();
-
-        this.scene.popMatrix();
+        this.buttonGroup.display();
 
         this.scene.popMatrix();
     }
