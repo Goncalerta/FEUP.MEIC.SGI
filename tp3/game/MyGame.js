a8e83e0a680fb5058ae3b41981f4a9bd77a88e2a
Diogo Costa <up201906731@edu.fe.up.pt>
2023-01-01 02:05:34 +0000

    Set player 1 POV at the start

--- tp3/game/MyGame.js
+++ tp3/game/MyGame.js
@@ -30,6 +30,7 @@ export class MyGame {
         this.scoreBoard = new MyScoreBoard(scene, this, this.cameras, this.player1, this.player2, playCallBack);
 
         this.crosses = new Set();
+        this.firstFrame = true;
     }
 
     setSpotlightPosition(x, y) {
@@ -83,6 +84,12 @@ export class MyGame {
     }
 
     display(pickMode) {
+        // TODO: any better place to do this to avoid checking every frame?
+        if (this.firstFrame) {
+            this.firstFrame = false;
+            this.cameras.setPlayerCamera(1);
+        }
+
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI / 2, 0, 1, 0);
         this.scene.translate(0, 1, -this.TILE_SIZE * (this.model.BOARD_SIZE / 2 + 2) );
